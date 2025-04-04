8fe13188d77ab5100d801857eb5f3d3a1ead44bf
Himani1609 <himanibansal1691998@gmail.com>
2025-04-04 15:00:42 -0400

    Modify fragment.html and script.js and Add images of chapters and fragments in images folder

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,19 +1,31 @@
-// Get the header element
 const header = document.getElementById("header");
+
 function getNavigation() {
-  // Create element
-  const navigation = document.createElement("nav");
-  // Set the element
-  navigation.innerHTML = `<nav class="navbar bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-      Hotel Levvie
-    </a>
-  </div>
-</nav>`;
-  // Append it to the parent
-  header.appendChild(navigation);
+  const title = document.createElement("h1");
+  title.id = "case-title"; 
+  title.textContent = "CASE FILES";
+  header.appendChild(title);
 }
 
-// Call the function
 getNavigation();
+
+
+// // Get the header element
+// const header = document.getElementById("header");
+// function getNavigation() {
+//   // Create element
+//   const navigation = document.createElement("nav");
+//   // Set the element
+//   navigation.innerHTML = `<nav class="navbar bg-light">
+//   <div class="container-fluid">
+//     <a class="navbar-brand" href="#">
+//       Hotel Levvie
+//     </a>
+//   </div>
+// </nav>`;
+//   // Append it to the parent
+//   header.appendChild(navigation);
+// }
+
+// // Call the function
+// getNavigation();
